de817a19bcd795f494e4244d2eb8d62b09f8d69b
Jhon63027 <jhonescorciacaraballo@gmail.com>
2024-12-06 11:44:44 -0500

    git commit "Mi sitio web"

---
+++ Lab4/script.js
@@ -0,0 +1,4 @@
+document.getElementById('ContacForm').addEventListener('submit',function(addEventListener){
+    event.preventDefault(); //eviytar que se recargue la pagina
+    alert('Formulario enviado correctamente gracias por contactarnos...!');
+});
